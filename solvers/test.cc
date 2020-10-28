9431c2f4de236746a21e300b36f5501941596af4
abdelmouttaleb <abdelmouttalebdakri@gmail.com>
2020-10-28 17:04:08 +0100

    ettaouchi

---
+++ solvers/test.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include "Dense"
+#include <fstream>
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+
+  Vector3d v;
+  Vector3d u;
+  u<<1.;
+  cout<<u<<endl;
+
+
+  return 0;
+}
