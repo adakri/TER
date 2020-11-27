e5874c74cfcae198c6d398eed15d8ecba0751d3f
adakri <71411281+adakri@users.noreply.github.com>
2020-11-27 11:12:37 +0100

    Update launcher.sh

--- imagz_seg/mesh testing is done here/launcher.sh
+++ imagz_seg/mesh testing is done here/launcher.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-FreeFem++ mesh.edp
+
+#encore à re-tester
+#lecture de l'image se fait automatiquement ./launcher.sh name_image
+
+
+if [ -f "$1"] ; then
+  cmake .
+  echo "cmake abouti"
+  make
+  echo "compilation sans erreurs"
+  ./Display_image $1
+  echo "execution complète"
+  FreeFem+ mesh.edp  
+elif [ -n "$1" ] ; then
+  echo "la valeur que vous avez entrer n'est pas valide"
+else
+  echo "ce fichier n'existe pas"
+fi
+echo "Vous avez choisi'limage à traiter: $1"
+read name_image
+
+
