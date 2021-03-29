92425817462ada27698516f9608073b1ec6c2612
adakri <71411281+adakri@users.noreply.github.com>
2021-03-29 13:46:30 +0200

    Update Solver.cpp

--- FE/Solver.cpp
+++ FE/Solver.cpp
@@ -1,25 +1,27 @@
 #ifndef _SOLVER_CPP
 
 #include "Solver.h"
+#include "Gradpo.h"
 #include <iostream>
 
 Solver::Solver() {}
 
 Solver::~Solver() {}
 
-void EigenSolver::setSystemMatrix(Eigen::SparseMatrix<double,Eigen::RowMajor> systemMatrix)
+void EigenSolver::setSystemMatrix(Eigen::SparseMatrix<double,Eigen::RowMajor> systemMatrix, Eigen::VectorXd&)
 {
   Eigen::SparseMatrix<double,Eigen::ColMajor> A(systemMatrix);
   // Compute the ordering permutation vector from the structural pattern of A
-  _solver.analyzePattern(A);
+  Gradpo Gradpo(MatrixXd A,VectorXd b,int kmax)
   // Compute the numerical factorization
-  _solver.factorize(A);
+  Eigen::VectorXd x(A.shape());
+  x=Gradpo.solve(A);
 }
 
 Eigen::SparseVector<double> EigenSolver::solve(Eigen::SparseVector<double> RHS)
 {
   //Use the factors to solve the linear system
-  return _solver.solve(RHS);
+  return Gradpo.solve(RHS);
 }
 
 #define _SOLVER_CPP
